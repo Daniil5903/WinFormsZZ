6104c9d811bf4a9049606ece7a3f12f6c238824b
Daniil5903 <danilka7518@gmail.com>
2025-03-20 12:44:00 +0300

    создал класс, который управляет доступными формулами и выполняет вычисления.

---
+++ MyLib/FormulaManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class FormulaManager
+    {
+        private List<PhysicalFormula> formulas = new List<PhysicalFormula>();
+
+        public void RegisterFormula(PhysicalFormula formula)
+        {
+            formulas.Add(formula);
+        }
+
+        public PhysicalFormula GetFormulaByName(string name)
+        {
+            return formulas.FirstOrDefault(f => f.Name == name);
+        }
+    }
+}
