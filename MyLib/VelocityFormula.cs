73e4fbe51ddb9024b2bcb8971f12f8f55bd8c01c
Daniil5903 <hiroesmen@gmail.com>
2025-03-22 10:07:15 +0300

    Класс с формулой скорости при равноускоренном движении

---
+++ MyLib/VelocityFormula.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class VelocityFormula : PhysicalFormula
+    {
+        public VelocityFormula()
+            : base("Скорость при равноускоренном движении", "v = v0 + a * t",
+                   new Dictionary<string, string> { { "v", "м/с" }, { "v0", "м/с" }, { "t", "с" }, { "a", "м/с²" } })
+        { }
+
+        public override double Calculate(Dictionary<string, double> parameters)
+        {
+            double v0 = parameters["v0"];
+            double t = parameters["t"];
+            double a = parameters["a"];
+            return v0 + a * t;
+        }
+    }
+
+}
