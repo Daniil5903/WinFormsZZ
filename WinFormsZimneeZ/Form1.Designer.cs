b2b7fa33f74fc5cbe7a3b903ec193f436add6d9d
Daniil5903 <hiroesmen@gmail.com>
2025-03-22 10:56:02 +0300

    сделал форму

--- WinFormsZimneeZ/Form1.Designer.cs
+++ WinFormsZimneeZ/Form1.Designer.cs
@@ -28,13 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtV0 = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.comboBoxFormulas = new System.Windows.Forms.ComboBox();
+            this.txtT = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtV0
+            // 
+            this.txtV0.Location = new System.Drawing.Point(12, 31);
+            this.txtV0.Name = "txtV0";
+            this.txtV0.Size = new System.Drawing.Size(100, 20);
+            this.txtV0.TabIndex = 0;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(151, 119);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(62, 13);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "Результат:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 119);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Рассчитать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // comboBoxFormulas
+            // 
+            this.comboBoxFormulas.FormattingEnabled = true;
+            this.comboBoxFormulas.Location = new System.Drawing.Point(154, 31);
+            this.comboBoxFormulas.Name = "comboBoxFormulas";
+            this.comboBoxFormulas.Size = new System.Drawing.Size(195, 21);
+            this.comboBoxFormulas.TabIndex = 3;
+            // 
+            // txtT
+            // 
+            this.txtT.Location = new System.Drawing.Point(12, 57);
+            this.txtT.Name = "txtT";
+            this.txtT.Size = new System.Drawing.Size(100, 20);
+            this.txtT.TabIndex = 4;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(12, 83);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtT);
+            this.Controls.Add(this.comboBoxFormulas);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtV0);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtV0;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ComboBox comboBoxFormulas;
+        private System.Windows.Forms.TextBox txtT;
+        private System.Windows.Forms.TextBox txtA;
     }
 }
 
--- WinFormsZimneeZ/Form1.cs
+++ WinFormsZimneeZ/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MyLib;
 
 namespace WinFormsZimneeZ
 {
@@ -15,6 +16,55 @@ namespace WinFormsZimneeZ
         public Form1()
         {
             InitializeComponent();
+            comboBoxFormulas.Items.Add("Равноускоренное движение");
+            comboBoxFormulas.Items.Add("Другая формула");
+            comboBoxFormulas.SelectedIndex = 0; // Установим первый элемент по умолчанию
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получаем значения из текстовых полей
+                double v0 = Convert.ToDouble(txtV0.Text);
+                double t = Convert.ToDouble(txtT.Text);
+                double a = Convert.ToDouble(txtA.Text);
+
+                // Выбираем нужную формулу
+                PhysicalFormula formula;
+                if (comboBoxFormulas.SelectedItem.ToString() == "Равноускоренное движение")
+                {
+                    formula = new UniformlyAcceleratedMotion();
+                }
+                else
+                {
+                    // Добавить другие формулы, когда они будут
+                    MessageBox.Show("Формула не реализована");
+                    return;
+                }
+
+                // Подготавливаем параметры
+                var parameters = new Dictionary<string, double>
+                {
+                    { "v0", v0 },
+                    { "t", t },
+                    { "a", a }
+                };
+
+                // Выполняем расчет
+                double result = formula.Calculate(parameters);
+                lblResult.Text = "Результат: " + result.ToString("F2");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
         }
     }
+    
 }
