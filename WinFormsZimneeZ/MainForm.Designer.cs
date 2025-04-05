7257cbab6f29b38571fd2adc78dc4f925fb16754
Daniil5903 <danilka7518@gmail.com>
2025-04-05 10:58:09 +0300

    переделал форму

--- WinFormsZimneeZ/MainForm.Designer.cs
+++ WinFormsZimneeZ/MainForm.Designer.cs
@@ -28,147 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtV0 = new System.Windows.Forms.TextBox();
-            this.lblResult = new System.Windows.Forms.Label();
-            this.btnCalculate = new System.Windows.Forms.Button();
-            this.comboBoxFormulas = new System.Windows.Forms.ComboBox();
-            this.txtT = new System.Windows.Forms.TextBox();
-            this.txtA = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
             this.treeViewFormulas = new System.Windows.Forms.TreeView();
             this.lblFormula = new System.Windows.Forms.Label();
+            this.lblV0 = new System.Windows.Forms.Label();
+            this.txtV0 = new System.Windows.Forms.TextBox();
+            this.lblT = new System.Windows.Forms.Label();
+            this.txtT = new System.Windows.Forms.TextBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // txtV0
-            // 
-            this.txtV0.Location = new System.Drawing.Point(580, 12);
-            this.txtV0.Name = "txtV0";
-            this.txtV0.Size = new System.Drawing.Size(100, 20);
-            this.txtV0.TabIndex = 0;
-            // 
-            // lblResult
-            // 
-            this.lblResult.AutoSize = true;
-            this.lblResult.Location = new System.Drawing.Point(736, 99);
-            this.lblResult.Name = "lblResult";
-            this.lblResult.Size = new System.Drawing.Size(62, 13);
-            this.lblResult.TabIndex = 1;
-            this.lblResult.Text = "Результат:";
-            // 
-            // btnCalculate
-            // 
-            this.btnCalculate.Location = new System.Drawing.Point(580, 100);
-            this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(100, 23);
-            this.btnCalculate.TabIndex = 2;
-            this.btnCalculate.Text = "Рассчитать";
-            this.btnCalculate.UseVisualStyleBackColor = true;
-            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
-            // 
-            // comboBoxFormulas
-            // 
-            this.comboBoxFormulas.FormattingEnabled = true;
-            this.comboBoxFormulas.Location = new System.Drawing.Point(739, 11);
-            this.comboBoxFormulas.Name = "comboBoxFormulas";
-            this.comboBoxFormulas.Size = new System.Drawing.Size(195, 21);
-            this.comboBoxFormulas.TabIndex = 3;
-            // 
-            // txtT
-            // 
-            this.txtT.Location = new System.Drawing.Point(580, 38);
-            this.txtT.Name = "txtT";
-            this.txtT.Size = new System.Drawing.Size(100, 20);
-            this.txtT.TabIndex = 4;
-            // 
-            // txtA
-            // 
-            this.txtA.Location = new System.Drawing.Point(580, 64);
-            this.txtA.Name = "txtA";
-            this.txtA.Size = new System.Drawing.Size(100, 20);
-            this.txtA.TabIndex = 5;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(429, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Нальная скорость";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(429, 41);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Время";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(429, 67);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Ускорение";
             // 
             // treeViewFormulas
             // 
-            this.treeViewFormulas.Location = new System.Drawing.Point(12, 12);
+            this.treeViewFormulas.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeViewFormulas.Location = new System.Drawing.Point(0, 0);
             this.treeViewFormulas.Name = "treeViewFormulas";
-            this.treeViewFormulas.Size = new System.Drawing.Size(366, 134);
+            this.treeViewFormulas.Size = new System.Drawing.Size(366, 199);
             this.treeViewFormulas.TabIndex = 9;
             // 
             // lblFormula
             // 
             this.lblFormula.AutoSize = true;
-            this.lblFormula.Location = new System.Drawing.Point(597, 236);
+            this.lblFormula.Location = new System.Drawing.Point(394, 3);
             this.lblFormula.Name = "lblFormula";
-            this.lblFormula.Size = new System.Drawing.Size(49, 13);
+            this.lblFormula.Size = new System.Drawing.Size(61, 13);
             this.lblFormula.TabIndex = 10;
-            this.lblFormula.Text = "Очистка";
+            this.lblFormula.Text = "Формула: ";
+            this.lblFormula.Visible = false;
+            // 
+            // lblV0
+            // 
+            this.lblV0.AutoSize = true;
+            this.lblV0.Location = new System.Drawing.Point(394, 50);
+            this.lblV0.Name = "lblV0";
+            this.lblV0.Size = new System.Drawing.Size(140, 13);
+            this.lblV0.TabIndex = 11;
+            this.lblV0.Text = "Начальная скорость (м/с)";
+            this.lblV0.Visible = false;
+            // 
+            // txtV0
+            // 
+            this.txtV0.Location = new System.Drawing.Point(397, 84);
+            this.txtV0.Name = "txtV0";
+            this.txtV0.Size = new System.Drawing.Size(137, 20);
+            this.txtV0.TabIndex = 12;
+            this.txtV0.Visible = false;
+            // 
+            // lblT
+            // 
+            this.lblT.AutoSize = true;
+            this.lblT.Location = new System.Drawing.Point(559, 50);
+            this.lblT.Name = "lblT";
+            this.lblT.Size = new System.Drawing.Size(55, 13);
+            this.lblT.TabIndex = 13;
+            this.lblT.Text = "Время (с)";
+            // 
+            // txtT
+            // 
+            this.txtT.Location = new System.Drawing.Point(562, 84);
+            this.txtT.Name = "txtT";
+            this.txtT.Size = new System.Drawing.Size(100, 20);
+            this.txtT.TabIndex = 14;
+            this.txtT.Visible = false;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(686, 50);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(94, 13);
+            this.lblA.TabIndex = 15;
+            this.lblA.Text = "Ускорение (м/с²)";
+            this.lblA.Visible = false;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(689, 84);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 16;
+            this.txtA.Visible = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(397, 146);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 17;
+            this.btnCalculate.Text = "Рассчитать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Visible = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(559, 146);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(65, 13);
+            this.lblResult.TabIndex = 18;
+            this.lblResult.Text = "Результат: ";
+            this.lblResult.Visible = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1312, 598);
+            this.ClientSize = new System.Drawing.Size(791, 199);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.txtT);
+            this.Controls.Add(this.lblT);
+            this.Controls.Add(this.txtV0);
+            this.Controls.Add(this.lblV0);
             this.Controls.Add(this.lblFormula);
             this.Controls.Add(this.treeViewFormulas);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtA);
-            this.Controls.Add(this.txtT);
-            this.Controls.Add(this.comboBoxFormulas);
-            this.Controls.Add(this.btnCalculate);
-            this.Controls.Add(this.lblResult);
-            this.Controls.Add(this.txtV0);
             this.Name = "MainForm";
             this.Text = "MainForm";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtV0;
-        private System.Windows.Forms.Label lblResult;
-        private System.Windows.Forms.Button btnCalculate;
-        private System.Windows.Forms.ComboBox comboBoxFormulas;
-        private System.Windows.Forms.TextBox txtT;
-        private System.Windows.Forms.TextBox txtA;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TreeView treeViewFormulas;
         private System.Windows.Forms.Label lblFormula;
+        private System.Windows.Forms.Label lblV0;
+        private System.Windows.Forms.TextBox txtV0;
+        private System.Windows.Forms.Label lblT;
+        private System.Windows.Forms.TextBox txtT;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
     }
 }
 
--- WinFormsZimneeZ/MainForm.cs
+++ WinFormsZimneeZ/MainForm.cs
@@ -16,78 +16,67 @@ namespace WinFormsZimneeZ
         public MainForm()
         {
             InitializeComponent();
-            comboBoxFormulas.Items.Add("Равноускоренное движение");
-            comboBoxFormulas.Items.Add("Другая формула");
-            comboBoxFormulas.SelectedIndex = 0; // Установим первый элемент по умолчанию
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void MainForm_Load(object sender, EventArgs e)
         {
-            // Основная категория
+            // Структура дерева
             TreeNode molecularNode = new TreeNode("Молекулярно-кинетическое движение");
-
-            // Подкатегория для Молекулярно-кинетического движения
             TreeNode mechanicalNode = new TreeNode("Механическое движение");
+            TreeNode uniformlyAcceleratedNode = new TreeNode("Равноускоренное движение");
 
-            // Подкатегория для Механического движения
-            TreeNode uniformAccelerationNode = new TreeNode("Равноускоренное движение");
-
-            // Добавляем основные категории в TreeView
+            // Вложенные узлы
             treeViewFormulas.Nodes.Add(molecularNode);
-
-            // Добавляем подкатегории в механическое движение
             molecularNode.Nodes.Add(mechanicalNode);
+            mechanicalNode.Nodes.Add(uniformlyAcceleratedNode);
 
-            // 
-            mechanicalNode.Nodes.Add(uniformAccelerationNode);
-
-            // Разворачиваем дерево
             treeViewFormulas.ExpandAll();
+
+            // Прячем элементы справа
+            SetCalculationControlsVisible(false);
+
+            // Подключаем двойной клик
+            treeViewFormulas.NodeMouseDoubleClick += treeViewFormulas_NodeMouseDoubleClick;
         }
 
-        private void treeViewFormulas_AfterSelect(object sender, TreeViewEventArgs e)
+        private void treeViewFormulas_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
         {
-            // Очищаем поле перед обновлением
-            lblFormula.Text = "";
-
-            // Определяем, какая формула выбрана
-            switch (e.Node.Text)
+            if (e.Node.Text == "Равноускоренное движение")
             {
-                case "Равноускоренное движение":
-                    lblFormula.Text = "s = v₀ * t + (1/2) * a * t²";
-                    break;
-                case "Другая формула":
-                    lblFormula.Text = "Формула для другой категории";
-                    break;
-                default:
-                    lblFormula.Text = "Выберите формулу для отображения";
-                    break;
+                lblFormula.Text = "s = v₀ * t + (1/2) * a * t²";
+                SetCalculationControlsVisible(true);
             }
+            else
+            {
+                SetCalculationControlsVisible(false);
+                lblFormula.Text = "Формула ещё не реализована.";
+            }
+
+            lblResult.Text = "";
+        }
+
+        private void SetCalculationControlsVisible(bool visible)
+        {
+            lblFormula.Visible = visible;
+            txtV0.Visible = visible;
+            txtT.Visible = visible;
+            txtA.Visible = visible;
+            lblV0.Visible = visible;
+            lblT.Visible = visible;
+            lblA.Visible = visible;
+            btnCalculate.Visible = visible;
+            lblResult.Visible = visible;
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             try
             {
-                // Получаем значения из текстовых полей
                 double v0 = Convert.ToDouble(txtV0.Text);
                 double t = Convert.ToDouble(txtT.Text);
                 double a = Convert.ToDouble(txtA.Text);
 
-                // Выбираем нужную формулу
-                PhysicalFormula formula;
-                if (comboBoxFormulas.SelectedItem.ToString() == "Равноускоренное движение")
-                {
-                    formula = new UniformlyAcceleratedMotion();
-                }
-                else
-                {
-                    // Добавить другие формулы, когда они будут
-                    MessageBox.Show("Формула не реализована");
-                    return;
-                }
-
-                // Подготавливаем параметры
+                var formula = new UniformlyAcceleratedMotion();
                 var parameters = new Dictionary<string, double>
                 {
                     { "v0", v0 },
@@ -95,9 +84,9 @@ namespace WinFormsZimneeZ
                     { "a", a }
                 };
 
-                // Выполняем расчет
+                MessageBox.Show("Начало расчёта"); // Временно для проверки
                 double result = formula.Calculate(parameters);
-                lblResult.Text = "Результат: " + result.ToString("F2");
+                MessageBox.Show("Результат: " + result.ToString("F2") + " м", "Готово", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (Exception ex)
             {
